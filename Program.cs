aded63ba826a1982c896657101f6440f3652afec
RMJTromp <me@rmjtromp.com>
2026-09-06 15:33:02 +0200

    Add main entry file

--- Program.cs
+++ Program.cs
@@ -1,6 +1,28 @@
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOpenApi("v1", options =>
+{
+    options.AddDocumentTransformer((doc, _, _) =>
+    {
+        doc.Info = new()
+        {
+            Title = "CargoHUB WMS",
+            Version = "1.0",
+        };
+        return Task.CompletedTask;
+    });
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// TODO: Add endpoint discovery
 
-app.Run();
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi("/openapi/{documentName}.json");   // -> /openapi/v1.json
+    app.MapScalarApiReference();                      // -> /scalar
+}
+
+await app.RunAsync();
